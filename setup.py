b82f3fd192d91ccf71e902cb67d0b4c09df51608
Zhengze Zhou <zhengzezhou@Zhengzes-MacBook-Pro.local>
2021-06-09 19:57:48 -0400

    fix import error

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='slime',
       author='Zhengze Zhou',
       author_email='zz433@cornell.edu',
       license='BSD',
-      packages=['slime'],
+      packages=['slime', 'lm'],
       python_requires='>=3.5',
       install_requires=[
           'lime',
