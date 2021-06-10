b834b9ca5c30d99639dc136175a1d341578a4d09
Zhengze Zhou <zhengzezhou@Zhengzes-MacBook-Pro.local>
2021-06-09 20:02:32 -0400

    fix import error

--- slime/lime_base.py
+++ slime/lime_base.py
@@ -5,7 +5,7 @@ import numpy as np
 import scipy as sp
 import sklearn
 from sklearn.linear_model import Ridge
-from . import _least_angle
+from lm import lars_path
 from sklearn.utils import check_random_state
 
 
